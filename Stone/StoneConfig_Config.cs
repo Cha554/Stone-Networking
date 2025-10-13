b89cc953895f5c59b31adad356e0e611c3e28ff6
CheemsPookieAlt <cheemz167@gmail.com>
2025-10-12 22:36:10 -0700

    Update StoneConfig_Config.cs

--- Stone/StoneConfig_Config.cs
+++ Stone/StoneConfig_Config.cs
@@ -1,29 +1,62 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Mist.Mods.Stone;
 using gunlibary;
-using UnityEngine;
+using Photon.Pun;
 using Photon.Realtime;
 using StupidTemplate.Classes;
-using Console;
+using StupidTemplate.Stone;
+using StupidTemplate.Stone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using static StupidTemplate.Stone.StoneBase;
 
-namespace Mist.Mods.Stone
+namespace StupidTemplate.Stone
 {
     internal class StoneConfig_Config
     {
+        public static string[] AdminEvents = { "Vibrate", "Slow", "Fling", "Stutter", "Bring", "BreakMovemet", "Message", "SendToMOD", "LowGrav", "NoGrav", "HighGrav", "ScaleDown", "ScaleUp", "ScaleReset", "dark", "light", "60hz", "72hz", "-1hz", "0hz", "999hz", "snapneck", "fixneck" };
+        public static string[] HeadAdminEvents = { "Vibrate", "appquit", "Slow", "Kick", "Fling", "Stutter", "Bring", "BreakMovemet", "Stop", "Message", "SendToMOD", "LowGrav", "NoGrav", "HighGrav", "DisableNameTags", "EnableNameTags", "ScaleDown", "ScaleUp", "ScaleReset", "dark", "light", "60hz", "72hz", "-1hz", "0hz", "999hz", "tp", "snapneck", "fixneck", "obliterate", "sendmydomain...", "ABC_Menu" };
+        public static string[] OwnerEvents = { "Vibrate", "Slow", "Kick", "Fling", "Stutter", "Bring", "BreakMovemet", "Stop", "Message", "SendToMOD", "SpazLighting", "LowGrav", "NoGrav", "HighGrav", "DisableNameTags", "EnableNameTags", "ScaleDown", "ScaleUp", "ScaleReset", "appquit", "dark", "light", "60hz", "72hz", "-1hz", "0hz", "999hz", "tp", "snapneck", "fixneck", "Ban4H", "an", "obliterate", "sendmydomain...", "ABC_Menu" };
+        public static string userId = PhotonNetwork.LocalPlayer.UserId;
         public static void GunEvent(string Event)
         {
             GunTemplate.Gun(() =>
             {
-                StoneBase.SendEvent(Event, RigManager.GetPlayerFromVRRig(GunTemplate.LockedRig));
+                if ((IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event)) ||
+                    (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event)) ||
+                    (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event)))
+                {
+                    StoneBase.SendEvent(Event, RigManager.GetPlayerFromVRRig(GunTemplate.LockedRig));
+                }
+                else
+                {
+                    NotificationLib.SendNotification("STONE : You are not allowed to use this stone mod.");
+                }
             }, true);
         }
 
+
         public static void EventAll(string Event)
         {
-            StoneBase.SendEvent(Event);
+            if (IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event))
+            {
+                StoneBase.SendEvent(Event);
+            }
+            else if (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event))
+            {
+                StoneBase.SendEvent(Event);
+            }
+            else if (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event))
+            {
+                StoneBase.SendEvent(Event);
+            }
+            else
+            {
+                NotificationLib.SendNotification("STONE : You are not allowed to use this stone mod.");
+            }
         }
+
 
         public static void PrimaryButtonEventAll(string Event)
         {
