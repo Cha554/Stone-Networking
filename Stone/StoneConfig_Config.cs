f98016d4c762f4b57c8fd3a086ad71242cb14019
Cha <113856355+Cha554@users.noreply.github.com>
2026-03-20 20:45:55 -0400

    Update StoneConfig_Config.cs

--- Stone/StoneConfig_Config.cs
+++ Stone/StoneConfig_Config.cs
@@ -1,35 +1,57 @@
-using gunlibary;
 using Photon.Pun;
-using Photon.Realtime;
-using StupidTemplate.Classes;
-using StupidTemplate.Notifications;
-using StupidTemplate.Stone;
-using StupidTemplate.Stone;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 using static StupidTemplate.Stone.StoneBase;
+using gunlibary;
+using Spectral_Menu.Notifications;
+using Spectral_Menu.Classes;
 
 namespace StupidTemplate.Stone
 {
     internal class StoneConfig_Config
     {
-        public static string HelperEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/HelperEvents").GetAwaiter().GetResult();
-        public static string AdminEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/AdminEvents").GetAwaiter().GetResult();
-        public static string HeadAdminEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/HeadAdminEvents").GetAwaiter().GetResult();
-        public static string OwnerEvents = new HttpClient().GetStringAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/OwnerEvents").GetAwaiter().GetResult();
-        public static string userId = PhotonNetwork.LocalPlayer.UserId;
+        public static string HelperEvents   = string.Empty;
+        public static string AdminEvents    = string.Empty;
+        public static string HeadAdminEvents = string.Empty;
+        public static string OwnerEvents    = string.Empty;
+
+        private static readonly HttpClient _http = new HttpClient { Timeout = System.TimeSpan.FromSeconds(5) };
+
+        public static async Task LoadWhitelistAsync()
+        {
+            var tasks = new[]
+            {
+                SafeGetAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/HelperEvents"),
+                SafeGetAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/AdminEvents"),
+                SafeGetAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/HeadAdminEvents"),
+                SafeGetAsync("https://raw.githubusercontent.com/Cha554/Stone-Networking/refs/heads/main/Stone/whitelist/OwnerEvents")
+            };
+
+            string[] results = await Task.WhenAll(tasks);
+            HelperEvents    = results[0];
+            AdminEvents     = results[1];
+            HeadAdminEvents = results[2];
+            OwnerEvents     = results[3];
+
+            Debug.Log("Stone: Whitelist events loaded.");
+        }
+
+        private static async Task<string> SafeGetAsync(string url)
+        {
+            try { return await _http.GetStringAsync(url); }
+            catch { return string.Empty; }
+        }
+
         public static void GunEvent(string Event)
         {
+            string userId = PhotonNetwork.LocalPlayer.UserId;
             GunTemplate.Gun(() =>
             {
-                if ((IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event)) ||
-                    (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event)) ||
-                    (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event)) ||
-                    (IsHelper(userId) && StoneConfig_Config.HelperEvents.Contains(Event)))
+                if ((IsOwner(userId)     && OwnerEvents.Contains(Event))    ||
+                    (IsHeadAdmin(userId) && HeadAdminEvents.Contains(Event)) ||
+                    (IsAdmin(userId)     && AdminEvents.Contains(Event))     ||
+                    (IsHelper(userId)    && HelperEvents.Contains(Event)))
                 {
                     StoneBase.SendEvent(Event, RigManager.GetPlayerFromVRRig(GunTemplate.LockedRig));
                 }
@@ -40,32 +62,21 @@ namespace StupidTemplate.Stone
             }, true);
         }
 
-
-
         public static void EventAll(string Event)
         {
-            if (IsOwner(userId) && StoneConfig_Config.OwnerEvents.Contains(Event))
+            string userId = PhotonNetwork.LocalPlayer.UserId;
+            if ((IsOwner(userId) && OwnerEvents.Contains(Event))         ||
+                (IsHeadAdmin(userId) && HeadAdminEvents.Contains(Event)) ||
+                (IsAdmin(userId) && AdminEvents.Contains(Event)))
             {
                 StoneBase.SendEvent(Event);
-            }
-            else if (IsHeadAdmin(userId) && StoneConfig_Config.HeadAdminEvents.Contains(Event))
-            {
-                StoneBase.SendEvent(Event);
-            }
-            else if (IsAdmin(userId) && StoneConfig_Config.AdminEvents.Contains(Event))
-            {
-                StoneBase.SendEvent(Event);
-            }
-            else
-            {
-                NotifiLib.SendNotification("STONE : You are not allowed to use this stone mod.");
             }
         }
 
-
         public static void PrimaryButtonEventAll(string Event)
         {
-            if (ControllerInputPoller.instance.rightControllerPrimaryButton || ControllerInputPoller.instance.leftControllerPrimaryButton)
+            if (ControllerInputPoller.instance.rightControllerPrimaryButton ||
+                ControllerInputPoller.instance.leftControllerPrimaryButton)
             {
                 StoneBase.SendEvent(Event);
             }
@@ -78,24 +89,22 @@ namespace StupidTemplate.Stone
 
         public static void Grab()
         {
-            foreach (VRRig rig in GorillaParent.instance.vrrigs)
+            foreach (VRRig rig in VRRigCache.ActiveRigs)
             {
-                if (rig != GorillaTagger.Instance.offlineVRRig)
-                {
-                    if (Vector3.Distance(GorillaTagger.Instance.rightHandTransform.position, rig.headMesh.transform.position) < 0.9f
-                        && ControllerInputPoller.instance.rightGrab)
-                    {
-                        StoneBase.SendEvent("GrabR", RigManager.GetPlayerFromVRRig(rig));
-                    }
+                if (rig == GorillaTagger.Instance.offlineVRRig) continue;
 
-                    if (Vector3.Distance(GorillaTagger.Instance.leftHandTransform.position, rig.headMesh.transform.position) < 0.9f
-                        && ControllerInputPoller.instance.leftGrab)
-                    {
-                        StoneBase.SendEvent("GrabL", RigManager.GetPlayerFromVRRig(rig));
-                    }
+                if (Vector3.Distance(GorillaTagger.Instance.rightHandTransform.position, rig.headMesh.transform.position) < 0.9f
+                    && ControllerInputPoller.instance.rightGrab)
+                {
+                    StoneBase.SendEvent("GrabR", Spectral_Menu.Classes.RigManager.GetPlayerFromVRRig(rig));
+                }
+
+                if (Vector3.Distance(GorillaTagger.Instance.leftHandTransform.position, rig.headMesh.transform.position) < 0.9f
+                    && ControllerInputPoller.instance.leftGrab)
+                {
+                    StoneBase.SendEvent("GrabL", Spectral_Menu.Classes.RigManager.GetPlayerFromVRRig(rig));
                 }
             }
         }
     }
 }
-
